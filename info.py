43fe3dcf759f1e95b3e66f5aea73192cff6cc6e1
nandishs177-a11y <nandishs177@gmail.com>
2026-03-14 01:47:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -266,7 +266,7 @@ Bot_cmds = {
     "remove_premium": "ʀᴇᴍᴏᴠᴇ ᴀɴʏ ᴜꜱᴇʀ ꜰʀᴏᴍ ᴘʀᴇᴍɪᴜᴍ.",
     "premium_users": "ɢᴇᴛ ʟɪꜱᴛ ᴏꜰ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀꜱ.",
     "get_premium": "ɢᴇᴛ ɪɴꜰᴏ ᴏꜰ ᴀɴʏ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀ.",
-    "restart": "ʀᴇꜱᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ."
+    "restart": "ʀᴇꜱᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ.",
     "genlink": "Store single message",
     "batch": "Store multiple channel messages",
     "custom_batch": "Store random messages"
