e0d7df2be476c9dda14c35cad429fd1c24a52f82
nandishs177-a11y <nandishs177@gmail.com>
2026-07-15 18:15:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -83,17 +83,17 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 # Verification Settings
 # ============================
 # Verification On (True) / Off (False)
-VERIFY = environ.get('VERIFY', 'False') == 'False'
+VERIFY = environ.get('VERIFY', 'True') == 'True'
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 1))  # Add time in hours
 VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1003800962005'))  # Log channel id (make sure bot is admin)
-HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/KNMoviesdiscussion/17')  # How to open tutorial link for verification
+HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/KNMoviesdiscussion/381')  # How to open tutorial link for verification
 
 # ============================
 # Link Shortener Configuration
 # ============================
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'arolinks.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'e9becab328d6bd69268889ead3d291d0fd9376e0')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'easysky.in')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'd951b20bd4a9352a81b1f653a41edb9f03038eaa')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_Download_7x/30')  # Tutorial video link for opening shortlink website
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', False))
 
