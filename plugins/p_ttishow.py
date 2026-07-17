dc751338c6c21861c648aa89bf915af4355f4ffd
nandishs177-a11y <nandishs177@gmail.com>
2026-07-17 17:45:20 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -4,8 +4,8 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from pyrogram.errors.exceptions.bad_request_400 import MessageTooLong, PeerIdInvalid
 from info import *
-from database.users_chats_db import db, db2
-from database.ia_filterdb import Media, Media2
+from database.users_chats_db import db
+from database.ia_filterdb import Media
 from utils import get_size, temp, get_settings, get_readable_time
 from Script import script
 from pyrogram.errors import ChatAdminRequired
@@ -171,11 +171,9 @@ async def get_ststs(bot, message):
     free = 536870912 - size
     size = get_size(size)
     free = get_size(free)
-    files = await Media2.count_documents()
-    size2 = await db2.get_db_size()
-    free2 = 536870912 - size2
-    size2 = get_size(size2)
-    free2 = get_size(free2)
+    files = 0
+    size2 = "0 B"
+    free2 = "0 B"
     uptime = get_readable_time(time() - botStartTime)
     ram = psutil.virtual_memory().percent
     cpu = psutil.cpu_percent()
