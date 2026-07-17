277e543f98324242752a0c9f52ab63d13302a350
nandishs177-a11y <nandishs177@gmail.com>
2026-07-17 21:33:40 +0530

    Update info.py

--- info.py
+++ info.py
@@ -85,7 +85,7 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 # Verification On (True) / Off (False)
 VERIFY = environ.get('VERIFY', 'True') == 'True'
 VERIFY_EXPIRE = int(environ.get('VERIFY_EXPIRE', 1))  # Add time in hours
-VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1003800962005'))  # Log channel id (make sure bot is admin)
+VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1004447059426'))  # Log channel id (make sure bot is admin)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/KNMoviesdiscussion/381')  # How to open tutorial link for verification
 
 # ============================
